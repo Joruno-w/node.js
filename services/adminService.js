f229c8e208cbb1bdc55853ecf1b210147a46986a
wangshengliang <1710642275@qq.com>
2020-09-22 19:30:34 +0800

    data select and md5 secret

--- services/adminService.js
+++ services/adminService.js
@@ -1,5 +1,7 @@
 const Admin = require('../models/Admin');
-exports.addAdmin = async function (adminObj) {
+const md5  = require('md5');
+exports.addAdmin = async function (adminObj){
+    adminObj.loginPwd = md5(adminObj.loginPwd);
     const ins = await Admin.create(adminObj);
     return ins.toJSON();
 }
@@ -18,9 +20,40 @@ exports.updateAdmin = async function (id,adminObj) {
     // const ins = await Admin.findByPk(id);
     // ins.loginId = adminObj.loginId;
     // await ins.save();
+    if (adminObj.loginPwd){
+        adminObj.loginPwd = md5(adminObj.loginPwd);
+    }
     await Admin.update(adminObj,{
        where: {
            id
        }
     });
+}
+
+
+exports.login = async function (loginId,loginPwd) {
+    loginPwd = md5(loginPwd);
+    const result = await Admin.findOne({
+        where: {
+            loginId,
+            loginPwd
+        }
+    });
+    if (result && result.loginId === loginId){
+        return result.toJSON();
+    }
+    return null;
+}
+
+
+exports.getAdminById = async function (id) {
+    const result = await Admin.findByPk({
+        where: {
+            id
+        }
+    });
+    if (result){
+        return result.toJSON();
+    }
+    return null;
 }
--- services/studentService.js
+++ services/studentService.js
@@ -1,4 +1,6 @@
 const Student = require('../models/Student');
+const {Op} = require('sequelize');
+const Class = require('../models/Class');
 exports.addStudent = async function (StudentObj) {
     const ins = await Student.create(StudentObj);
     return ins.toJSON();
@@ -24,3 +26,33 @@ exports.updateStudent = async function (id,StudentObj) {
         }
     });
 }
+
+exports.getAllStudents = async function () {
+    const result = await Student.findAll();
+    return JSON.stringify(result);
+}
+
+exports.getStudentByPage = async function (page=1,limit=10,sex=-1,name) {
+    const where = {}
+    if (sex!==-1){
+        where.sex = !!sex;
+    }
+    if (name){
+        where.name = {
+            [Op.like]: `%${name}%`
+        }
+    }
+    const result = await Student.findAndCountAll({
+        attributes: ['id','name','sex','birthday'],
+        where,
+        include: [Class],
+        offset: (page - 1) * limit,
+        limit: +limit
+    });
+    return {
+        total: result.count,
+        datas: JSON.parse(JSON.stringify(result.rows))
+    }
+
+}
+
