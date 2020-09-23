d5587b2811f39a6bc5887c923f0508b8cb4245bf
wangshengliang <1710642275@qq.com>
2020-09-23 21:49:23 +0800

    use nodemon

--- index.js
+++ index.js
@@ -4,18 +4,7 @@ const port = 5008;
 app.listen(port,()=>{
     console.log(`正在监听${port}`);
 });
-app.get('/news/:id',(req,res)=>{
-    console.log(req.header);
-    console.log(req.path);
-    console.log(req.query);
-    console.log(req.params);
-    // res.send('123');
-    // res.send([1,2,3]);
-    // res.send({
-    //     a: 1,
-    //     b: 2
-    // });
-    // res.status(302).header('location','http://www.baidu.com').end();
-    // res.status(302).location('http://www.baidu.com').end();
-    res.redirect(302,'http://www.baidu.com');
+
+app.get("*",()=>{
+    console.log("abc123");
 });
