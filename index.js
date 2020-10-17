aeae0b53959fce6c3e90356375e89f0e480ce408
wangshengliang <1710642275@qq.com>
2020-10-17 15:51:58 +0800

    picture defender

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = 5008;
 app.listen(port, () => {
     console.log(`正在监听${port}`);
 });
-
+app.use(require('./routes/imgProtectMid'));
 app.use(cors({
     origin(origin, callback) {
         if (!origin) {
---
+++ routes/imgProtectMid.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const url = require('url');
+module.exports = (req,res,next)=>{
+    const host = req.headers.host;
+    let referer = req.headers.referer;
+    const extname = path.extname(req.path);
+    if (![".jpg",'.png','.gif','.jpeg'].includes(extname)) {
+        next();
+        return;
+    }
+    if (referer){
+        referer = url.parse(referer,true).host;
+    }
+    if (referer && referer !== host) {
+        req.url = '/img/2.jpg';
+    }
+    next();
+}
