61209882709a83e0146d8bdc608644685fa94710
wangshengliang <1710642275@qq.com>
2020-10-17 15:09:01 +0800

    picture watermark

--- routes/api/upload.js
+++ routes/api/upload.js
@@ -2,9 +2,24 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const path = require('path');
+const jimp = require('jimp');
+async function mark(waterFile,originFile,targetFile,proportion=5,marginProportion=0.05){
+    const [origin,water] = await Promise.all([jimp.read(originFile),jimp.read(waterFile)]);
+    const curProportion = origin.bitmap.width / water.bitmap.width;
+    water.scale(curProportion / proportion);
+    const right = origin.bitmap.width * marginProportion;
+    const bottom = origin.bitmap.height * marginProportion;
+    const x = origin.bitmap.width - right - water.bitmap.width;
+    const y = origin.bitmap.height - bottom - water.bitmap.height
+    origin.composite(water,x,y,{
+        mode: jimp.BLEND_SOURCE_OVER,
+        opacitySource: 0.3
+    });
+    await origin.write(targetFile);
+}
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null,path.resolve(__dirname,'../../public'));
+        cb(null,path.resolve(__dirname,'../../public/origin'));
     },
     filename: function (req, file, cb) {
         const timeStamp = Date.now();
@@ -14,14 +29,18 @@ const storage = multer.diskStorage({
     }
 })
 
-const upload = multer({ storage: storage })
-
-router.post("/", upload.single("img"),(req, res, next) => {
+const upload = multer({ storage })
+const waterPath = path.resolve(__dirname,'../../public/img/water.jpg');
+router.post("/", upload.single("img"),async (req, res, next) => {
+    //添加水印
+    const targetPath = path.resolve(__dirname,'../../public',req.file.filename);
+    await mark(waterPath,req.file.path,targetPath);
     res.send({
         code: 200,
         msg: '',
         data: req.file.filename
     });
+    next();
 });
 
 module.exports = router;
